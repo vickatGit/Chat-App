18ed1eb9ef3e01eb55085856664c7b2125856f56
vikas <gangishettyvikas@gmail.com>
2022-08-03 16:34:34 +0530

    viewpage function for login and sigup is done

---
+++ app/src/main/java/com/example/chat_app/Adapters/ViewPagerAdapter.kt
@@ -0,0 +1,25 @@
+package com.example.chat_app.Adapters
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import androidx.fragment.app.FragmentManager
+import androidx.lifecycle.Lifecycle
+import androidx.recyclerview.widget.RecyclerView
+import androidx.viewpager2.adapter.FragmentStateAdapter
+import com.example.chat_app.Fragments.LoginFragment
+import com.example.chat_app.Fragments.SignupFragment
+
+class ViewPagerAdapter(fragmentManager: FragmentManager, lifecycle: Lifecycle) :
+    FragmentStateAdapter(fragmentManager, lifecycle) {
+    override fun getItemCount(): Int {
+        return 2;
+    }
+
+    override fun createFragment(position: Int): Fragment {
+        when(position){
+            0 ->return LoginFragment()
+            1 -> return SignupFragment()
+            else -> return SignupFragment()
+        }
+    }
+}
--- app/src/main/java/com/example/chat_app/MainActivity.kt
+++ app/src/main/java/com/example/chat_app/MainActivity.kt
@@ -2,10 +2,39 @@ package com.example.chat_app
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.FragmentActivity
+import androidx.viewpager2.widget.ViewPager2
+import com.example.chat_app.Adapters.ViewPagerAdapter
+import com.google.android.material.tabs.TabItem
+import com.google.android.material.tabs.TabLayout
+import com.google.android.material.tabs.TabLayoutMediator
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var tablayout:TabLayout
+    private lateinit var viewpager:ViewPager2
+    private lateinit var loginSignupAdapter:ViewPagerAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initialise()
+        loginSignupAdapter= ViewPagerAdapter(supportFragmentManager,lifecycle)
+        viewpager.adapter=loginSignupAdapter
+        TabLayoutMediator(tablayout,viewpager){tab,position->
+            if (position==0)
+                tab.setText("Login")
+            else
+                tab.setText("Signup")
+
+        }.attach()
+
+
+    }
+
+    private fun initialise() {
+        tablayout=findViewById(R.id.login_signup_tab)
+        viewpager=findViewById(R.id.login_signup_viewpager)
     }
 }
