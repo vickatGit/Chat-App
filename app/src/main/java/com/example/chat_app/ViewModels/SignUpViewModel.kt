6ca0d30bd804f721a4722a56b702d2686e93146f
vikas <gangishettyvikas@gmail.com>
2022-08-04 18:08:47 +0530

    viewmodel(Repository logic) is implemented for signup

--- app/src/main/java/com/example/chat_app/ViewModels/SignUpViewModel.kt
+++ app/src/main/java/com/example/chat_app/ViewModels/SignUpViewModel.kt
@@ -1,27 +1,36 @@
 package com.example.chat_app.ViewModels
 
+import android.util.Log
+import androidx.annotation.Nullable
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.chat_app.Network.Network.User
 import com.example.chat_app.Repository.RestRepo
+import java.util.logging.Handler
 
 
-class LoginViewModel: ViewModel() {
+class SignUpViewModel: ViewModel() {
 
 
     private lateinit var repo:RestRepo
     public  var users:MutableLiveData<Int> = MutableLiveData()
 
     init {
-        repo= RestRepo()
+        repo= RestRepo.getInstance()
     }
-    fun createUser(user:User): MutableLiveData<Int> {
-        users=repo.createUser(user)
+    companion object{
+
+    }
+    fun createUser(user: User): MutableLiveData<Int> {
+        users.postValue(repo.createUser(user).value)
+        while(users==null){
+
+        }
+        Log.d("TAG", "create user viewmodel"+users.value)
         return users
     }
     fun getUser(): MutableLiveData<Int> {
-        users=repo.getUser()
-        return users
+        return repo.getUser()
     }
 
 }
